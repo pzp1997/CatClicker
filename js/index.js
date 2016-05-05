467d36797c4f0a178787b95066534bcaaa87f28a
Palmer Paul <pzpaul2002@yahoo.com>
2016-05-05 16:50:10 -0400

    Multiple cats!

--- js/index.js
+++ js/index.js
@@ -1,7 +1,48 @@
-var $catImg = document.getElementsByClassName('cat-img')[0],
-    $counter = document.getElementsByClassName('counter')[0],
-    count = 0;
+var cats = [{
+    name: "Tom",
+    imgSrc: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Tiggy_the_talking_cat.JPG/1024px-Tiggy_the_talking_cat.JPG",
+    counter: 0
+}, {
+    name: "Captain McFurry",
+    imgSrc: "https://newevolutiondesigns.com/images/freebies/cat-wallpaper-preview-24.jpg",
+    counter: 0
+}, {
+    name: "Snowball",
+    imgSrc: "http://mypetforumonline.com/wp-content/uploads/2014/08/fat-cat.jpg",
+    counter: 0
+}, ];
 
-$catImg.addEventListener('click', function () {
-  $counter.innerHTML = ++count;
-});
+var createCatElem = function(cat) {
+    var container, nameLabel, catImg, counterLabel;
+
+    container = document.createElement("div");
+    container.className = "container text-center";
+
+    nameLabel = document.createElement("h2");
+    nameLabel.className = "name";
+    nameLabel.innerHTML = cat.name;
+
+    catImg = document.createElement("img");
+    catImg.className = "cat-img";
+    catImg.alt = "Cat Image";
+    catImg.src = cat.imgSrc;
+
+    counterLabel = document.createElement("h2");
+    counterLabel.className = "counter";
+    counterLabel.innerHTML = cat.counter;
+
+    catImg.addEventListener("click", function() {
+        counterLabel.innerHTML = ++cat.counter;
+    }, false);
+
+    container.appendChild(nameLabel);
+    container.appendChild(catImg);
+    container.appendChild(counterLabel);
+
+    return container;
+};
+
+var $content = document.getElementById("content");
+for (var i = 0; i < cats.length; i++) {
+    $content.appendChild(createCatElem(cats[i]));
+}
